46f60ffc2502dc30131fb28752afe1c4dbf7fca3
dongx._.2 <dongdong99@kakao.com>
2024-10-09 17:01:43 +0900

    WIP on main

---
+++ my-website/.docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("/Users/dongh/my-website/node_modules/infima/dist/css/default/default.css"),
+  require("/Users/dongh/my-website/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/Users/dongh/my-website/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/Users/dongh/my-website/src/css/custom.css"),
+];

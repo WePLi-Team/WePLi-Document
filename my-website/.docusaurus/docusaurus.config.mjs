b4dc5a597e0ed72347d771b9fea7d8ae75b68e04
dongx._.2 <dongdong99@kakao.com>
2024-10-09 18:26:36 +0900

    파비콘 변경

--- my-website/.docusaurus/docusaurus.config.mjs
+++ my-website/.docusaurus/docusaurus.config.mjs
@@ -4,7 +4,7 @@
  * Modify the docusaurus.config.js file at your site's root instead.
  */
 export default {
-  "title": "My Site",
+  "title": "WePLi",
   "tagline": "Dinosaurs are cool",
   "favicon": "img/favicon.ico",
   "url": "https://wepli-test.github.io",

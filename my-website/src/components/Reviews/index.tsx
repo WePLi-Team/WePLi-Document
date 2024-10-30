e7ec7c578c3827c35deba66333d6db6b19fb3d45
dongx._.2 <dongdong99@kakao.com>
2024-10-30 12:31:08 +0900

    리뷰 패딩 수정

--- my-website/src/components/Reviews/index.tsx
+++ my-website/src/components/Reviews/index.tsx
@@ -34,7 +34,7 @@ const posts = [
         },
     },
     {
-        id: 2,
+        id: 3,
         title: 'Boost your conversion rate',
         href: '#',
         description:
@@ -64,7 +64,7 @@ export default function ReviewSection() {
 
                 <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
                     {posts.map((post) => (
-                        <article key={post.id} className="flex max-w-xl flex-col items-start justify-between">
+                        <article key={post.id} className="flex max-w-xl flex-col items-start justify-between p-4">
                             <div className="flex items-center gap-x-4 text-xs">
                                 <time dateTime={post.datetime} className="text-gray-500">
                                     {post.date}
